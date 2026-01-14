3ecb09c2e99c8d6d61d2144370df5045d3e83b76
Dutch0903 <joostflorijn@gmail.com>
2026-01-14 15:53:44 +0100

    feat: use hey-api openapi ts to generate sdk based on the openapi specs from the api

---
+++ ui/openapi-ts.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "@hey-api/openapi-ts";
+
+export default defineConfig({
+  input: "http://localhost:8090/openapi",
+  logs: {
+    level: "info",
+  },
+  output: {
+    clean: true,
+    format: "prettier",
+    list: "eslint",
+    path: "./src/api",
+    tsConfigPath: "off",
+  },
+  plugins: [
+    {
+      name: "@hey-api/schemas",
+      nameBuilder: (name) => `${name}Schema`,
+      type: "json",
+      exportFromIndex: true,
+    },
+    {
+      name: "@hey-api/client-fetch",
+      throwOnError: true,
+      strictBaseUrl: true,
+    },
+    {
+      name: "@hey-api/sdk",
+      exportFromIndex: true,
+    },
+    {
+      name: "@hey-api/typescript",
+      case: "PascalCase",
+      enums: "javascript",
+    },
+  ],
+});
